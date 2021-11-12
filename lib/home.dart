a1fba61fea6f676149b44ff2f7c6fe334500c08b
ANDREYDEN <andrey2850@gmail.com>
2021-11-12 10:32:14 -0500

    Add bottom navigation

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_recipes/welcome.dart';
 import 'package:image_picker/image_picker.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -15,6 +16,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   XFile? _uploadedFile;
   final ImagePicker _picker = ImagePicker();
+  int _pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +24,43 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_uploadedFile == null)
-              const Text("Waiting for upload")
-            else
-              kIsWeb
-                  ? Image.network(_uploadedFile!.path)
-                  : Image.file(File(_uploadedFile!.path)),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_camera),
+            label: "Image picker",
+          ),
+        ],
+        currentIndex: _pageIndex,
+        onTap: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _uploadImage,
-        tooltip: 'Upload File',
-        child: const Icon(Icons.image),
-      ),
+      body: _pageIndex == 0
+          ? const WelcomePage()
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (_uploadedFile == null)
+                    const Text("Waiting for upload")
+                  else
+                    kIsWeb
+                        ? Image.network(_uploadedFile!.path)
+                        : Image.file(File(_uploadedFile!.path)),
+                ],
+              ),
+            ),
+      floatingActionButton: _pageIndex == 0
+          ? null
+          : FloatingActionButton(
+              onPressed: _uploadImage,
+              tooltip: 'Upload File',
+              child: const Icon(Icons.image),
+            ),
     );
   }
 
